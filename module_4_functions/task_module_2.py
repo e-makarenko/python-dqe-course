b0eaae02c3037832e72f20cdc3d9f9d5594556e3
Emilia Makarenko <emilia.makarenko@altrata.com>
2024-03-05 11:25:42 +0100

    HW for Module 4 corrected

--- module_4_functions/task_module_2.py
+++ module_4_functions/task_module_2.py
@@ -1,11 +1,6 @@
 import random
 
 
-def create_random_number_of_dicts(min_val=2, max_val=10):
-    number_of_dicts = random.randint(min_val, max_val)
-    return number_of_dicts
-
-
 def create_random_dict(min_key_val=1, max_key_val=26, min_value=ord('a'), max_value=ord('z')):
     dict_keys = random.randint(min_key_val, max_key_val)
     return {chr(random.randint(min_value, max_value)): random.randint(0, 100) for _ in range(dict_keys)}
@@ -42,15 +37,16 @@ def create_final_dict(keys_with_max_values, new_dict):
     return final_dict_sorted
 
 
-dicts_number = create_random_number_of_dicts()
-print("Randomly generated number of dictionaries that will be stored in the list: ", dicts_number)
+if __name__ == "__main__":
+    dicts_number = random.randint(2, 10)
+    print("Randomly generated number of dictionaries that will be stored in the list: ", dicts_number)
 
-list_of_dictionaries = [create_random_dict() for _ in range(dicts_number)]
-print("\nList with dictionaries:", list_of_dictionaries)
+    list_of_dictionaries = [create_random_dict() for _ in range(dicts_number)]
+    print("\nList with dictionaries:", list_of_dictionaries)
 
-keys_with_max_values_list, new_dict = find_duplicate_keys_and_their_max_values(list_of_dictionaries)
-print("\nList of keys which repeat in dictionaries: ", keys_with_max_values_list)
-print("\nNew dictionary: ", new_dict)
+    keys_with_max_values_list, new_dict = find_duplicate_keys_and_their_max_values(list_of_dictionaries)
+    print("\nList of keys which repeat in dictionaries: ", keys_with_max_values_list)
+    print("\nNew dictionary: ", new_dict)
 
-final_dictionary = create_final_dict(keys_with_max_values_list, new_dict)
-print("\nFinal new dictionary (sorted): ", final_dictionary)
+    final_dictionary = create_final_dict(keys_with_max_values_list, new_dict)
+    print("\nFinal new dictionary (sorted): ", final_dictionary)
--- module_4_functions/task_module_3.py
+++ module_4_functions/task_module_3.py
@@ -5,17 +5,9 @@ def split_by_special_characters(text):
     return re.split(r'([.!?]\s*)', text)
 
 
-def convert_to_lowercase(text):
-    return text.lower()
-
-
 # function to capitalize sentences based on their index in splitted sentences (even index = sentence)
 def capitalize_sentences(sentences):
     return [sentence.strip().capitalize() if i % 2 == 0 else sentence for i, sentence in enumerate(sentences)]
-
-
-def replace_iz_with_is(text):
-    return text.replace(' iz ', ' is ')
 
 
 def count_whitespaces(text):
@@ -23,35 +15,35 @@ def count_whitespaces(text):
 
 
 def process_text(text):
-    lowercase_text = convert_to_lowercase(text)
-    sentences = split_by_special_characters(lowercase_text)
+    text = text.lower()
+    sentences = split_by_special_characters(text)
     print(sentences)
     formatted_sentences = capitalize_sentences(sentences)
     formatted_text = ''.join(formatted_sentences)
-    corrected_text = replace_iz_with_is(formatted_text)
+    corrected_text = formatted_text.replace(' iz ', ' is ')
 
     return corrected_text
 
 
-original_text = '''homEwork:
+if __name__ == "__main__":
+    original_text = '''homEwork:
+    
+      tHis iz your homework, copy these text to variable.
+    
+    
+    
+      You need to normalize it from letter cases point of view. also, create one more sentence with last words of each existing sentence and add it to the end of this paragraph.
+    
+    
+    
+      it iz misspelling here. fix“iz” with correct “is”, but only when it iz a mistake.
+    
+    
+    
+      last iz to calculate number of whitespace characters in this tex. carefull, not only spaces, but all whitespaces. I got 87.'''
 
-  tHis iz your homework, copy these text to variable.
+    processed_text = process_text(original_text)
+    print(processed_text)
 
-
-
-  You need to normalize it from letter cases point of view. also, create one more sentence with last words of each existing sentence and add it to the end of this paragraph.
-
-
-
-  it iz misspelling here. fix“iz” with correct “is”, but only when it iz a mistake.
-
-
-
-  last iz to calculate number of whitespace characters in this tex. carefull, not only spaces, but all whitespaces. I got 87.'''
-
-
-processed_text = process_text(original_text)
-print(processed_text)
-
-whitespace_count = count_whitespaces(original_text)
-print(f'The original text contains {whitespace_count} whitespace characters.')
+    whitespace_count = count_whitespaces(original_text)
+    print(f'The original text contains {whitespace_count} whitespace characters.')
